1033fcb7781cd8297d7839f2018e77c47c488a66
Wbck <meertmrv@gmail.com>
2021-09-23 11:16:35 +0300

    First Commit

---
+++ Deneme/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Deneme
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Isminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name +" " + surname);
+        }
+    }
+}
