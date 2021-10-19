f3e732db54b7b64246916eded1392f53915dd08b
Wbck <meertmrv@gmail.com>
2021-10-19 14:25:13 +0300

    First Commit

---
+++ C#101-Odev1/4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lutfen bir cumle giriniz: ");
+            string metinDizisi = Console.ReadLine();
+
+            int toplamHarf = 0;
+
+            foreach (var karakter in metinDizisi)
+            {
+                toplamHarf++;
+            }
+            Console.WriteLine("Harf sayisi: "+toplamHarf);
+
+
+            string [] kelimeler = metinDizisi.Split(' ');
+            Console.WriteLine("Kelime sayisi: "+kelimeler.Length);
+            
+        }
+    }
+}
