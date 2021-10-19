0e03ead65b0d7362e824f9637398162b950c61c3
Wbck <meertmrv@gmail.com>
2021-10-19 13:45:45 +0300

    First Commit

---
+++ C#101-Odev1/3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lutfen pozitif bir sayi giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            string [] metinDizisi = new string [diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lutfen {0}. metiniz giriniz:");
+                metinDizisi[i] = Console.ReadLine(); 
+            }
+            Array.Reverse(metinDizisi);
+            Console.WriteLine("Girdiginiz metinler:");
+            foreach (var metin in metinDizisi)
+            {
+                Console.WriteLine(metin);
+            }
+        }
+    }
+}
