022863142d7e6b6af04b3bf612cff57a403d9514
Wbck <meertmrv@gmail.com>
2022-01-07 10:53:50 +0300

    Firt Commit

--- Sinif-Kavrami/Program.cs
+++ Sinif-Kavrami/Program.cs
@@ -6,7 +6,7 @@ namespace Sinif_Kavrami
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mert");
         }
     }
 }
