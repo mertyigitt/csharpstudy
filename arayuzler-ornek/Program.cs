8a5767d3fc09e764cdfbbf1d238110fc2546957c
Wbck <meertmrv@gmail.com>
2022-02-01 14:56:21 +0300

    Firs

---
+++ arayuzler-ornek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace arayuzler_ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Focus focus = new();
+            Console.WriteLine(focus.HangiMarkaninAraci().ToString());
+            Console.WriteLine(focus.KacTekerlektenOlusur());
+            Console.WriteLine(focus.StandartRengiNe().ToString());
+
+            Civic civic = new();
+            Console.WriteLine(civic.HangiMarkaninAraci().ToString());
+            Console.WriteLine(civic.KacTekerlektenOlusur());
+            Console.WriteLine(civic.StandartRengiNe().ToString());
+
+            Corolla corolla = new();
+            Console.WriteLine(corolla.HangiMarkaninAraci().ToString());
+            Console.WriteLine(corolla.KacTekerlektenOlusur());
+            Console.WriteLine(corolla.StandartRengiNe().ToString());
+        }
+    }
+}
---
+++ arayuzler/SmsLogger.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace arayuzler
+{
+    public class SmsLogger : ILogger
+    {
+        public void writeLog()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("Sms olarak yazar.");
+        }
+    }
+
+}
